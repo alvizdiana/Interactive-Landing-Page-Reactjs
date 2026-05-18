1146806b08cbe219b639636c9c4bd6360c921639
Alvi Izdiana <alfiizdiana@gmail.com>
2026-05-18 14:34:48 +0700

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Interactive-Landing-Page-Reactjs/',
+  base: './',
   plugins: [react(), tailwindcss()],
   build: {
     outDir: 'dist',
